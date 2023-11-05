4b27e2e1ae9f7e8b9402aae88807ff93d63dbc63
cyberail <barbaqadzedato3@gmail.com>
2023-11-05 16:06:01 +0400

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+    my_func(23);
+
+    println!("return value {}", returnable());
+
+    println!("return value {}", plus_one(3));
+}
+
+fn my_func(x: i32) {
+    println!("My func {x}");
+}
+
+fn returnable() -> i32 {
+    let f = 32;
+    f
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
